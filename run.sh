26247a92a1dbc1b658422c395709a47b5e161b6b
TUAT-Novice <164989847+TUAT-Novice@users.noreply.github.com>
2024-05-31 12:57:59 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ export nnodes=1
 export node_rank=0
 
 python -m torch.distributed.launch --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank  ./main.py \
-  --data-path D:/data/BraTS2018/MICCAI_BraTS_2018_Data_Training
+  --data-path path_to_BraTS_dataset
