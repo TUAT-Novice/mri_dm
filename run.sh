f1c7f956619cf82e9cb68132692932958d74684d
TUAT-Novice <164989847+TUAT-Novice@users.noreply.github.com>
2024-06-15 16:20:20 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
-export nproc_per_node=1
+export nproc_per_node=4
 export nnodes=1
 export node_rank=0
 
+epoch=1000
+batch_size=8
+accm_freq=1
+mri_ratio=2
+model_path='./saved_models/'  # './saved_models/MODEL_NAME.h5' # to continue
+
 python -m torch.distributed.launch --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank  ./main.py \
-  --data-path path_to_BraTS_dataset
+  --data-path /home/data1501/lbh/data/MICCAI_BraTS_2020_Data_Training/MICCAI_BraTS2020_TrainingData \
+  -n $epoch \
+  -b $batch_size \
+  --accumulate-step $accm_freq \
+  --mri_ratio $mri_ratio \
+  --model_path $model_path
+
